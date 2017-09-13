aded8eaece6718dee9ef45f562d5e8e41c76a589
REFAZUL REFAT <refazul.refat@gmail.com>
2017-09-14 03:03:46 +0600

    lynda code org

--- ext/js/background.js
+++ ext/js/background.js
@@ -2,7 +2,7 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.greeting == 'create_new_tab') {
         if (request.url) {
-            var url = url_query_param_add(request.url, 'tab_generated', 'true');
+            var url = utl_url_query_param_add(request.url, 'tab_generated', 'true');
             if (url) {
                 setTimeout(function() {
                     chrome.tabs.create({
@@ -13,12 +13,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         }
     }
 });
-function url_query_param_add(url, key, value) {
+function utl_url_query_param_add(url, key, value) {
     if (url) {
         if (url.indexOf('?') > -1) {
             url = url + '&' + key + '=' + value;
-        }
-        else {
+        } else {
             url = url + '?' + key + '=' + value;
         }
     }
@@ -26,7 +25,9 @@ function url_query_param_add(url, key, value) {
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.sendMessage(tab.id, {greeting: "lynda_open_links"}, function(response) {
+    chrome.tabs.sendMessage(tab.id, {
+        greeting: "open_links"
+    }, function(response) {
         console.log(response);
     });
 });
--- ext/js/content.js
+++ ext/js/content.js
@@ -1,6 +1,5 @@
 var host_url = 'https://139.59.118.97/grabber/';
-
-console.log(host_url);
+console.log('>>', host_url);
 
 if (location.href.indexOf('teamtreehouse.com') > -1) {
     var timer1;
@@ -38,45 +37,65 @@ if (location.href.indexOf('teamtreehouse.com') > -1) {
 } else if (location.href.indexOf('lynda.com') > -1) {
     // Lynda
     // https://lynda_files2-a.akamaihd.net/secure/courses/167065/VBR_MP4h264_main_SD/167065_02_02_MM30_how.mp4?c3.ri=3774749779903443748&hashval=1504645074_d1e97d2edd2779a158ed9b0e1763583f
-    var timer1;
-    clearInterval(timer1);
-    timer1 = setInterval(function() {
+    var lynda_timer;
+    clearInterval(lynda_timer);
+    lynda_timer = setInterval(function() {
         $('video').each(function() {
             var mp4_source = $(this).attr('data-src');
             if (mp4_source && mp4_source.indexOf('/secure/courses/') > -1) {
-                clearInterval(timer1);
+                clearInterval(lynda_timer);
                 var dir_name = lynda_dir_name_get(this);
-                lynda_post_video(dir_name, mp4_source);
+                console.log('>>', dir_name);
+                grabber_save_video('lynda', dir_name, mp4_source);
             }
         });
     }, 1000);
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        if (request.greeting == "lynda_open_links")
+        if (request.greeting == "open_links") {
+            lynda_open_links();
             sendResponse({status: "Opening Links"});
-        lynda_open_links();
+        }
     });
 }
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
 function lynda_dir_name_get(video_element) {
+    //$('#sidebar .toc-video-item[data-video-id="179445"]').parent().siblings('.chapter-row').find('[data-ga-label="toc-chapter"]').text()
     var dir_name = false;
-    var video_info = JSON.parse($(video_element).attr('data-conviva'));
+    var video_info = false;
+    try {
+        video_info = JSON.parse($(video_element).attr('data-conviva'));
+    } catch (e) {}
+
+    console.log('>>', video_info);
     if (video_info && parseInt(video_info.VideoId) > 0) {
         var video_id = video_info.VideoId;
         var course_title = video_info.CourseTitle;
         var chapter_title = $('#sidebar .toc-video-item[data-video-id="' + video_id + '"]').parent().siblings('.chapter-row').find('[data-ga-label="toc-chapter"]').text();
         var video_title = ($('#sidebar .toc-video-item[data-video-id="' + video_id + '"]').prevAll().length + 1) + '. ' + video_info.VideoTitle;
+        console.log('>>', video_id, course_title, chapter_title, video_title);
         var dir_name = course_title + '/' + chapter_title + '/' + video_title;
-        dir_name = sanitize(dir_name);
+        dir_name = utl_string_sanitize(dir_name);
     }
     return dir_name;
-    //$('#sidebar .toc-video-item[data-video-id="179445"]').parent().siblings('.chapter-row').find('[data-ga-label="toc-chapter"]').text()
 }
-function sanitize(string) {
+function lynda_open_links() {
+    $('#toc-content .video-row a').each(function(a, b) {
+        var url = $(b).attr('href');
+        send_message_to_background({greeting: 'create_new_tab', url: url});
+    });
+}
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+function utl_string_sanitize(string) {
     if (string) {
         string = string.replace(/[^a-zA-Z0-9\-. \/]/gi, '');
     };
     return string;
 }
-function getParameterByName(name, url) {
+function utl_url_param_get_by_name(name, url) {
     if (!url)
         url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -88,29 +107,39 @@ function getParameterByName(name, url) {
         return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-function lynda_post_video(dir_name, mp4_source) {
-    if (dir_name && mp4_source && getParameterByName('tab_generated') == 'true') {
-        console.log(dir_name, mp4_source);
-        $.ajax({
-            url: host_url + 'download',
-            method: 'POST',
-            data: {
-                source: 'lynda',
-                dir_name: dir_name,
-                mp4_source: mp4_source
-            },
-            success: function() {
-                window.close();
-            }
-        });
-    }
-}
-function lynda_open_links() {
-    $('#toc-content .video-row a').each(function(a, b) {
-        var url = $(b).attr('href');
-        send_message_to_background({greeting: 'create_new_tab', url: url});
+function send_message_to_background(message, callback) {
+    chrome.runtime.sendMessage(message, function(response_ext) {
+        if (typeof callback === 'function') {
+            callback(response_ext);
+        }
     });
 }
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+function grabber_save_video(source, dir_name, mp4_source) {
+    var allowed_sources = ['lynda', 'treehouse'];
+    if (allowed_sources.indexOf(source) > -1 && dir_name && mp4_source && utl_url_param_get_by_name('tab_generated') == 'true') {
+        console.log('>>', source, dir_name, mp4_source);
+        if (dir_name.length > 1 && mp4_source.length > 1) {
+            $.ajax({
+                url: host_url + 'download',
+                method: 'POST',
+                data: {
+                    source: source,
+                    dir_name: dir_name,
+                    mp4_source: mp4_source
+                },
+                success: function() {
+                    window.close();
+                }
+            });
+        }
+    }
+}
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
 function treehouse_open_links_1() {
     $('.featurette ul a').each(function(a, b) {
         var class_names = $(b).find('> svg').attr('class');
@@ -128,7 +157,6 @@ function treehouse_open_links_2() {
         send_message_to_background({greeting: 'create_new_tab', url: url});
     });
 }
-
 function treehouse_download_video() {
     var dir_name = location.href.split('/library/')[1];
     var mp4_source = $('video source[type="video/mp4"]').attr('src');
@@ -145,11 +173,4 @@ function treehouse_download_video() {
         }
     });
 }
-
-function send_message_to_background(message, callback) {
-    chrome.runtime.sendMessage(message, function(response_ext) {
-        if (typeof callback === 'function') {
-            callback(response_ext);
-        }
-    });
-}
+////////////////////////////////////////////////////////////////////////////////
